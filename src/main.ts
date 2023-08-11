c380b47a34a50c44c431fb0f3e6fd4e23454f35a
= <=>
2023-08-11 19:04:57 +0000

    codemirrow enhanced

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
-// enableProdMode();
+enableProdMode();
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch((err) => console.error(err));
